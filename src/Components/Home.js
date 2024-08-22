edf6cbb2abd57a735cd47cca29143d86b41650db
SAI NILESH <sainilesh67@gmail.com>
2024-08-22 19:48:45 +0530

    -final

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -87,6 +87,11 @@ const Home = () => {
       <div>
         <p className="text-2xl px-2 py-2 text-white font-semibold  ">Denomination</p>
       <p className="text-2xl px-2 py-2 pb-4  text-orange-400 font-semibold">Total Amount&nbsp;: &nbsp;{Amount}<br/></p>
+      <div className="text-center my-4">
+        <p className="text-orange-400 font-semibold text-xl ">{word.charAt(0).toUpperCase() + word.slice(1)}</p>
+            <p className="text-white font-semibold text-xl my-2">Total Notes :&nbsp;&nbsp;{notes}</p>
+            
+        </div>
       </div>
         <div className="border grid  grid-cols-custom  gap-2 bg-white text-black text-center  text-xl px-1 py-2">
           <div className="px-2 py-1">
@@ -261,11 +266,7 @@ const Home = () => {
             <p className="font-semibold">{OsAmount}</p>
           </div>
         </div>
-        <div className="text-center my-4">
-        <p className="text-orange-400 font-semibold text-xl ">{word.charAt(0).toUpperCase() + word.slice(1)}</p>
-            <p className="text-white font-semibold text-xl my-2">Total Notes :&nbsp;&nbsp;{notes}</p>
-            
-        </div>
+        
       </div>
     </div>
   );
